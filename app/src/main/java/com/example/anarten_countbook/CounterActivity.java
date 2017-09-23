421919eeca37aa11caa6367ae05f5b7f1b5e9b76
anarten <anarten@ualberta.ca>
2017-09-23 12:08:50 -0600

    Changed to CounterActivity

--- app/src/main/java/com/example/anarten_countbook/CounterActivity.java
+++ app/src/main/java/com/example/anarten_countbook/CounterActivity.java
@@ -3,11 +3,15 @@ package com.example.anarten_countbook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Adam on 9/23/2017.
+ */
 
+public class CounterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
